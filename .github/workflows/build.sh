5c181dd65115c6c62ef73bd7df0f16cb1fe714ce
Josh Long <josh@joshlong.com>
2022-01-10 15:41:58 -0800

    customer repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,5 +4,6 @@ cd $GITHUB_WORKSPACE
 echo "Starting build"
 mkdir -p $HOME/.m2/
 cp .ci.settings.xml $HOME/.m2/settings.xml
+docker pull mongo:5.0.3
 mvn -e -f $GITHUB_WORKSPACE/pom.xml spring-javaformat:apply  verify   deploy
 echo "Stopping build"
