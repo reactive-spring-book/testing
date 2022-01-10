46a2b58571258c7719655051e3157453e0df62ff
Josh Long <josh@joshlong.com>
2022-01-10 15:53:31 -0800

    testing

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,8 +4,8 @@ cd $GITHUB_WORKSPACE
 echo "Starting build"
 mkdir -p $HOME/.m2/
 cp .ci.settings.xml $HOME/.m2/settings.xml
-docker pull mongo:5.0.3
-docker pull testcontainers/ryuk:0.3.0
-docker pull alpine:3.5
+#docker pull mongo:5.0.3
+#docker pull testcontainers/ryuk:0.3.0
+#docker pull alpine:3.5
 mvn -e -f $GITHUB_WORKSPACE/pom.xml spring-javaformat:apply  verify   deploy
 echo "Stopping build"
